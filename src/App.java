7c30ee7272ff1bdad88eeaa654dde05988ee6995
gabriel <gaamdal557@gmail.com>
2022-07-25 20:02:58 -0300

    imagens com api-gabriel

--- src/App.java
+++ src/App.java
@@ -8,17 +8,22 @@ public class App {
 
   public static void main(String[] args) throws Exception {
 
-    API minhaAPI = API.MEME;
+    //API minhaAPI = API.MEME;
+
+    String url = "https://linguagens-api-gabriel.herokuapp.com/linguagens";
+    ExtratorDeConteudo extrator = new ExtratorDeConteudoDoIMDB();
 
     var http = new ClienteHttp();
-    String json = http.buscaDados(minhaAPI.url());
+   // String json = http.buscaDados(minhaAPI.url());
+     String json = http.buscaDados(url);
 
     // exibir e manipular os dados
-    List<Conteudo> conteudos = minhaAPI.extrator().extraiConteudos(json);
-
+    // List<Conteudo> conteudos = minhaAPI.extrator().extraiConteudos(json);
+    List<Conteudo> conteudos = extrator.extraiConteudos(json);
+    
     var geradora = new GeradoraDeFigurinhas();
 
-    for (int i = 0; i < 3; i++) {
+    for (int i = 0; i < conteudos.size() ; i++) {
 
       Conteudo conteudo = conteudos.get(i);
 
--- src/GeradoraDeFigurinhas.java
+++ src/GeradoraDeFigurinhas.java
@@ -1,51 +1,51 @@
-import java.awt.Color;
-import java.awt.Font;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
 import java.io.InputStream;
 import javax.imageio.ImageIO;
+import java.awt.Color;
+import java.awt.Font;
 
 public class GeradoraDeFigurinhas {
 
-    public void cria(InputStream InputStream, String nomeArquivo) throws IOException {
+    public void cria(InputStream InputStream, String nomeArquivo) throws Exception {
+
+        // leitura da imagem
+
+        // InputStream inputStream =
+
+        // new FileInputStream(new File("entrada/filme-maior.jpg"));
+
+        // InputStream inputStream =
+
+        // new
+        // URL("https://m.media-amazon.com/images/M/MV5BMDFkYTc0MGEtZmNhMC00ZDIzLWFmNTEtODM1ZmRlYWMwMWFmXkEyXkFqcGdeQXVyMTMxODk2OTU@.jpg")
+
+        // .openStream();
 
         BufferedImage imagemOriginal = ImageIO.read(InputStream);
 
-        // cria nova imagem em memória com transparência e com tamanho novo
+       // cria nova imagem em memória com transparência e com tamanho novo
+       int largura = imagemOriginal.getWidth();
+       int altura = imagemOriginal.getHeight();
+       int novaAltura = altura + 500;
+       BufferedImage novaImagem = new BufferedImage(largura, novaAltura, BufferedImage.TRANSLUCENT);
 
-        int largura = 800;
-        int altura = 1600;
-        BufferedImage novaImagem = new BufferedImage(largura, altura, BufferedImage.TRANSLUCENT);
+       // copiar a imagem original para nova imagem (em memória)
+       Graphics2D graphics = (Graphics2D) novaImagem.getGraphics();
+       graphics.drawImage(imagemOriginal, 0, 250, null);
 
-        // copiar a imagem original pra novo imagem (em memória)
+       // configurar a fonte
+       var fonte = new Font(Font.SANS_SERIF, Font.BOLD, 60);
+       graphics.setColor(Color.magenta);
+       graphics.setFont(fonte);
 
-        Graphics2D graphics = (Graphics2D) novaImagem.getGraphics();
-        graphics.drawImage(imagemOriginal, 0, 0, largura, altura - 300, null);
+       // escrever uma frase na nova imagem
+       graphics.drawString("TOP D+", 25, novaAltura - 200);
 
-        // configurar a fonte
-
-        Font fonte = new Font(Font.SANS_SERIF, Font.BOLD, 94);
-        graphics.setColor(Color.YELLOW);
-        graphics.setFont(fonte);
-
-        // imprimir uma foto
-
-        InputStream foto = new FileInputStream("entrada/gabriel_foto.png");
-        BufferedImage fotoOriginal = ImageIO.read(foto);
-        graphics.drawImage(fotoOriginal, 350, altura - 700, 550, 700, null);
-
-        // escrever uma frase na nova imagem
-
-        graphics.drawString("TOPEZERA", largura / 12, altura - 100);
-
-        // escrever a nova imagem em um arquivo
-
-        ImageIO.write(novaImagem, "png", new File(nomeArquivo));
-
+   
+       // escrever a nova imagem em um arquivo
+       ImageIO.write(novaImagem, "png", new File(nomeArquivo));
     }
-
 
 }
